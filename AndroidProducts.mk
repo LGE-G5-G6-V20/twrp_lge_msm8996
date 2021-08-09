56e85bc2ecc7291b3c6ab03bc1bdbc1d9ec992e4
Michael Bardoň <wolfovelp@gmail.com>
2021-08-09 12:52:59 +0200

    h872: TWRPify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_h872.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_h872-userdebug \
+    omni_h872-eng  
--- omni_h872.mk
+++ omni_h872.mk
@@ -17,7 +17,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := h872
 
-$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, build/target/product/full.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
