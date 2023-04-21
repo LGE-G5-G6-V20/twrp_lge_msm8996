e4d03561c786157b4e5d023d9782f93b3ff4c783
Bernardo Bas <bernardobas@gmail.com>
2023-04-21 14:19:10 -0300

    h830: remove reboot recovery button from the reboot menu
    May bring in the TWRP splash freeze bug

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_NO_EXFAT_FUSE := true
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_NO_REBOOT_RECOVERY := true
 
 # Ignore LG's bootloader wipe commands
 TW_IGNORE_MISC_WIPE_DATA := true
