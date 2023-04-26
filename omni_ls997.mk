4b054dc395f21eb97710b5643069333c3bbb150c
Bernardo Bas <bernardobas@gmail.com>
2023-04-26 16:45:59 -0300

    ls997: add timezone data

--- omni_ls997.mk
+++ omni_ls997.mk
@@ -11,6 +11,19 @@ PRODUCT_PACKAGES += \
 	charger_res_images \
 	charger
 
+# Define time zone data path
+ifneq ($(wildcard bionic/libc/zoneinfo),)
+    TZDATAPATH := bionic/libc/zoneinfo
+else ifneq ($(wildcard system/timezone),)
+    TZDATAPATH := system/timezone/output_data/iana
+endif
+
+# Time Zone data for Recovery
+ifdef TZDATAPATH
+PRODUCT_COPY_FILES += \
+    $(TZDATAPATH)/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+endif
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ls997
 PRODUCT_MODEL := LG-LS997
@@ -18,4 +31,3 @@ PRODUCT_NAME := omni_ls997
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_COPY_FILES += device/lge/$(PRODUCT_DEVICE)/recovery.fstab:recovery/root/etc/twrp.flags
-PRODUCT_COPY_FILES += bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
